49035ed5da4aa971ecb104f181fc2ba9415f1258
Ashish Sheth <ashishsheth@gmail.com>
2022-04-07 11:11:28 +0530

    first contract

---
+++ contracts/Greeter.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >= 0.8.9;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Greeter is Ownable {
+    string private _greeting = "Hello, World!";
+
+    function greet() external view returns(string memory) {
+        return _greeting;
+    }
+
+    function setGreeting(string calldata greeting) external onlyOwner {
+        _greeting = greeting;
+    }
+}
